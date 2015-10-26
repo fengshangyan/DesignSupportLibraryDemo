307c6c9f8adc9f5926676506253f29b19d99d3e8
fengshangyan <fengshangyan@fulan.com.cn>
2015-10-26 14:34:42 +0800

    Initial commit

---
+++ app/src/main/java/com/hujiang/designsupportlibrarydemo/ListFragment.java
@@ -0,0 +1,72 @@
+package com.hujiang.designsupportlibrarydemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ListFragment extends Fragment {
+
+    private RecyclerView mRecyclerView;
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mRecyclerView =
+                (RecyclerView) inflater.inflate(R.layout.list_fragment, container, false);
+        return mRecyclerView;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mRecyclerView.setLayoutManager(new GridLayoutManager(mRecyclerView.getContext(), 2));
+        mRecyclerView.setAdapter(new RecyclerViewAdapter(getActivity()));
+    }
+
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
